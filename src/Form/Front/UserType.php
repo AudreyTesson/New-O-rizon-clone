9b9f048555678bb2f552040cae680ac000570fd0
Christelle-Hidoine <christelle.hidoine@gmail.com>
2023-06-07 19:56:45 +0200

    userController creation with sign-in method to allow a user to registrate in the website. Creating form and initiate template to render the form

---
+++ src/Form/Front/UserType.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Form\Front;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Regex;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('firstname', TextType::class, ['label' => 'Prénom'])
+            ->add('lastname', TextType::class, ['label' => 'Nom'])
+            ->add('username', TextType::class, ['label' => 'Pseudonyme'])
+            ->add('email', EmailType::class, [
+                "attr" => ["placeholder" => "user@newOrizon.com"]
+            ])
+            ->add('password', RepeatedType::class,[
+            'type' => PasswordType::class, 
+            'options' => ['attr' => ['class' => 'password-field']],
+            'first_options' => [ 
+                    "label" => "Mot de passe",
+                    'constraints' => [
+                        new NotBlank(),
+                        new Regex(
+                            "/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/",
+                            "Le mot de passe doit contenir au minimum 8 caractères, une majuscule, un chiffre et un caractère spécial"
+                        ),
+                    ],
+                ],
+            'second_options' => [
+                'label' => 'Répétez le mot de passe',
+                ],
+            'invalid_message' => 'Les 2 mots de passe doivent être identiques'
+            ])
+        ;
+    }
+    
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
