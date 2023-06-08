4e5485b454632c48509aeea7287ed71c6bd9373a
XavierGauzere <xavier.gauzere@gmail.com>
2023-06-08 22:53:06 +0200

    display of carousel for mobile version is Ok

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,18 @@ module.exports = {
     'templates/**/*.html.twig',
     'templates/**/**/*.html.twig',
     'assets/js/**/*.js',
+    "./src/**/*.{html,js}",
+    "./node_modules/tw-elements/dist/js/**/*.js",
+    "./node_modules/flowbite/**/*.js"
   ],
   theme: {
     extend: {},
   },
   plugins: [
     require('@tailwindcss/forms'),
+    require("tw-elements/dist/plugin.cjs"),
+    require('flowbite/plugin'),
   ],
+  darkMode: "class"
 }
 
