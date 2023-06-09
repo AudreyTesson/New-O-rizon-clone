7027fe23e6238b183c510bb7517f2bb5874bfbf6
AudreyTesson <audrey.tesson.pro@outlook.fr>
2023-06-09 09:55:19 +0200

    city page: background, cityName + country to modify, slide with conditions: work in progress

---
+++ src/Controller/Front/CityController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller\Front;
+
+use App\Repository\CityRepository;
+use Exception;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CityController extends AbstractController
+{
+    /**
+     * City Page
+     * 
+     * @Route("/cities/{id}", name="cities_detail", requirements={"id":"\d+"})
+     */
+    public function show($id, CityRepository $cityRepository): Response
+    {
+        $city = $cityRepository->find($id);
+        if ($city === null) {
+            throw new Exception("Cette ville n'existe pas", 404);
+        }
+
+        return $this->render('front/cities/show.html.twig', [
+            'cityId' => $id,
+            'city' => $city,
+        ]);
+    }
+}
