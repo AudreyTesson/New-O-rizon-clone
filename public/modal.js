fa0978d71ff0d1181bbecf30d60f340abbd9b253
Christelle-Hidoine <christelle.hidoine@gmail.com>
2023-06-11 22:25:21 +0200

    bugfix close modal method with js on registration form

--- public/modal.js
+++ public/modal.js
@@ -1,29 +1,17 @@
 const modal = {
 
-    init: async function() {
-                
-        // form selector
-        const form = document.querySelector('form');
-        // event listener on form submit
-        form.addEventListener('submit', modal.handleModal);
-    },
-
-    /**
-     * Method to handle modal 
-     */
-    handleModal: function()
-    {
-        // close button selector
+    init: function(){
+        if (document.querySelector(".modal")) {
         const close = document.querySelector(".close_modal");
-        // event listener on close button click
+        // event listener on close modal button
         close.addEventListener('click', modal.closeModal);
+        }
     },
 
     /**
      * Method to close the modal on click event
      */
     closeModal: function(){
-        debugger;
         const modalSuccess = document.querySelector(".modal");
         modalSuccess.classList.add("hidden");
     },
