c4b00b5e79e7ea9d6fab748e83aedebf44d7f555
AudreyTesson <audrey.tesson.pro@outlook.fr>
2023-06-13 11:34:58 +0200

    map integration in cities/id page

---
+++ public/map.js
@@ -0,0 +1,17 @@
+<script>
+// Creating map options
+    var mapOptions = {
+    center: [17.385044, 78.486671],
+    zoom: 10
+    }
+
+// Creating a map object
+    var map = new L.map(‘map’, mapOptions);
+
+// Creating a Layer object
+    var layer = new L.TileLayer(‘http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png’);
+
+// Adding layer to the map
+    map.addLayer(layer);
+
+</script>
