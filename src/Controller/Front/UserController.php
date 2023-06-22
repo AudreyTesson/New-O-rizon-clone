d6dde6da13e2b905e8b73d296f643cd07a0c085f
Christelle-Hidoine <christelle.hidoine@gmail.com>
2023-06-22 23:14:39 +0200

    adding editing profile, display profile

--- src/Controller/Front/UserController.php
+++ src/Controller/Front/UserController.php
@@ -15,7 +15,7 @@ class UserController extends AbstractController
 {
     /**
      * Registration user method
-     * @Route("/sign-in", name="sign-in")
+     * @Route("/sign-in", name="sign-in", methods={"GET", "POST"})
      * 
      * @return Response
      */
@@ -48,6 +48,61 @@ class UserController extends AbstractController
             'user' => $user,
             'form' => $form,
         ]);
+    }
+
+    /**
+     * Display user profile connected
+     * @Route("/user/{id}", name="user_show", methods={"GET"}, requirements={"id"="\d+"})
+     */
+    public function show(User $user): Response
+    {
+        return $this->render('front/user/show.html.twig', [
+            'user' => $user,
+        ]);
+    }
+
+    /**
+     * Edit user profile connected
+     * @Route("/user/{id}/edit", name="user_edit", methods={"GET", "POST"}, requirements={"id"="\d+"})
+     */
+    public function edit(Request $request, User $user, UserRepository $userRepository): Response
+    {
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $userRepository->add($user, true);
+
+            return $this->redirectToRoute('default', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('front/user/edit.html.twig', [
+            'user' => $user,
+            'form' => $form,
+        ]);
+    }
+
+    /**
+     * Delete user profile connected
+     * @Route("/user/{id}", name="user_delete", methods={"DELETE"}, requirements={"id"="\d+"})
+     */
+    public function delete(Request $request, User $user, UserRepository $userRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$user->getId(), $request->request->get('_token'))) {        
+
+            $this->container->get('security.token_storage')->setToken(null);
+
+            $userRepository->remove($user, true);
+            
+            $this->addFlash(
+                'success',
+                "Votre compte a bien été supprimé, revenez quand vous voulez."
+            );
+
+            return $this->redirectToRoute('default', [], Response::HTTP_SEE_OTHER);
+        }
+        
+        
     }
 
 }
