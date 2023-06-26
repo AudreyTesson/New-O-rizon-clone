a44a94df3229ec8a056d2cf00f3154605ddb32a2
Christelle-Hidoine <christelle.hidoine@gmail.com>
2023-06-26 14:42:19 +0200

    fixing errors in console js

--- public/js/map.js
+++ public/js/map.js
@@ -5,10 +5,10 @@
     }
 
 // Creating a map object
-    var map = new L.map(‘map’, mapOptions);
+    var map = new L.map('map', mapOptions);
 
 // Creating a Layer object
-    var layer = new L.TileLayer(‘http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png’);
+    var layer = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
 
 // Adding layer to the map
     map.addLayer(layer);
