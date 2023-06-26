6ec0403f9a9de21bf866dead8160858d6c2b5ae5
AudreyTesson <audrey.tesson.pro@outlook.fr>
2023-06-26 21:15:27 +0200

    reviviews and user template forms for backoffice modified for buttons

--- src/Controller/Back/ReviewController.php
+++ src/Controller/Back/ReviewController.php
@@ -3,7 +3,7 @@
 namespace App\Controller\Back;
 
 use App\Entity\Review;
-use App\Form\ReviewType;
+use App\Form\Front\ReviewType;
 use App\Repository\ReviewRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
